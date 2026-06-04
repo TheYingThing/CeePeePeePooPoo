bd5efe8de0d8f10031095ba6b5a8fb26c10e46ca
Ying-Ling Dang <ying-ling.dang@bbv.ch>
2026-06-04 14:46:38 +0200

    okay more stuff

--- iJustNeedAFile.cpp
+++ iJustNeedAFile.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+template <typename T> T myMin(T y, T x){
+    return (x < y) ? x : y;
+}
+
 int main() {
-    cout << "Hello World";
+    cout << "Hello World\n";
+    cout << myMin(2, 3);
+
     return 0;
 }
