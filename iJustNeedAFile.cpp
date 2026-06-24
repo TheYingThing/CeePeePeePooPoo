f8960ded4c083c465620324c3c22a3b55dfed6b5
Ying-Ling Dang <ying-ling.dang@bbv.ch>
2026-06-24 15:40:01 +0200

    sure

--- iJustNeedAFile.cpp
+++ iJustNeedAFile.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> T myMin(T y, T x){
+template <typename T> T myMin(const T& y, const T& x){
     return (x < y) ? x : y;
 }
 
-int main() {
-    cout << "Hello World\n";
-    cout << myMin(2, 3);
+template <typename Container, typename Index> auto authAndAccess(Container& c, Index i) -> decltype(c[i]) {
+    
+}
 
+int main() {
+
+
+    cout << "Hello World\n";
+    cout << myMin(2, 3) << "\n";
+    cout << myMin('a', 'r');
+    cout << "\n";
     return 0;
 }
